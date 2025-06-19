68000792cd228fd4eb890996636e3594dbf5e683
MarloRRosa <docsrosas@gmail.com>
2025-06-19 19:41:21 -0300

    ajusterota

--- routes/admin.js
+++ routes/admin.js
@@ -1869,8 +1869,14 @@ router.get('/relatorio/avaliacao-offline/:feiraId/:avaliadorId', verificarAdminE
             req.flash('error_msg', 'Avaliador não encontrado ou não pertence a esta escola.');
             return res.redirect('/admin/dashboard?tab=relatorios');
         }
-        
-            let projetosQuery = { 
+
+        console.log('--- DEPURANDO RELATÓRIO DE AVALIADOR ---');
+console.log('ID do Avaliador recebido na rota:', avaliadorId);
+console.log('Nome do Avaliador encontrado:', avaliador.nome);
+console.log('Projetos Atribuídos (IDs crus do banco de dados):', avaliador.projetosAtribuidos);
+console.log('Quantidade de projetos atribuídos no Avaliador:', avaliador.projetosAtribuidos ? avaliador.projetosAtribuidos.length : 0);
+
+        let projetosQuery = { 
             feiraId: feira._id, 
             escolaId: adminEscolaId,
             _id: { $in: avaliador.projetosAtribuidos || [] }
@@ -1882,6 +1888,9 @@ router.get('/relatorio/avaliacao-offline/:feiraId/:avaliadorId', verificarAdminE
                                       .populate('categoria')
                                       .lean();
 
+        console.log('Projetos encontrados pela query (títulos):', projetos.map(p => p.titulo));
+console.log('Quantidade de projetos encontrados pela query:', projetos.length);
+console.log('--- FIM DA SEÇÃO DE DEPURACÃO ---');
 
         const categoriaIds = [...new Set(projetos.map(p => p.categoria && p.categoria._id).filter(Boolean))];
         const criteriosPorCategoria = {};
