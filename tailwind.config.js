683b46ac72e10b224970f8b86f25b5a47ab192f8
MarloRRosa <docsrosas@gmail.com>
2025-06-21 18:32:48 -0300

    ajuste css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./views/**/*.{ejs,js,html}", // <-- Adicione esta linha
-    // Se você tiver outras pastas com HTML ou JS onde usa classes Tailwind, adicione-as aqui também.
-    // Exemplo: "./public/**/*.js",
-    // "./public/**/*.html",
-  ],
+  content: ["./views/**/*.ejs", "./public/**/*.js"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
