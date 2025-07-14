68d3ec3c443e2d639ffbb581e53706fd37b39bf5
MarloRRosa <docsrosas@gmail.com>
2025-07-13 22:01:38 -0300

    Demo 2

---
+++ models/Usuario.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const UsuarioSchema = new mongoose.Schema({
+  nome: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  senha: {
+    type: String,
+    required: true
+  },
+  tipo: {
+    type: String,
+    enum: ['admin', 'superadmin', 'avaliador', 'demo'],
+    default: 'admin'
+  },
+  ativo: {
+    type: Boolean,
+    default: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Usuario', UsuarioSchema);
